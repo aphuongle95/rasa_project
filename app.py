862a059142beddd9f590beff15fea27d51ff6f18
Anh Phuong Le <annie.le@vier.ai>
2024-07-26 17:23:42 +0200

    init

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, redirect, url_for, request, render_template
+import requests
+import json
+import logging
+
+app = Flask(__name__, template_folder='./')
+context_set = ""
+
+
+response = [] # A list that will store the conversation
+
+# # Your solution here
+    
+def without_keys(d, keys):
+    return {x: d[x] for x in d if x not in keys}
+
+
+# @app.route("/")
+# def stories():
+#     return render_template('graph.html')
+
+@app.route('/', methods = ['GET'])
+def index():
+    response.clear()
+    return render_template('index.html', val ='')
+
+@app.route('/msg', methods = ['POST'])
+def msg():
+
+    val = str(request.form['text'])
+    data = json.dumps({"sender": "Rasa","message": val})
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    res = requests.post('http://localhost:5005/webhooks/rest/webhook', data= data, headers = headers)
+
+    if not val or val[0] == '/':
+        return render_template('index.html', val = response)
+    else:
+        res = res.json()
+        message = ''
+        response.append(val)
+        for x in res:
+            payload = list(without_keys(x, 'recipient_id').values())
+            message = message + "  " + str(payload[0])
+        response.append(message)
+        return render_template('index.html', val=response)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5006)
